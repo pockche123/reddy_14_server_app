c688e665961658cf483be375bb44433dc5f0e1e6
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-11-06 14:09:03 +0000

    some fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require("cors")
 const quiz = require('./quiz.json')
+const scores = require('./score.json')
 const app = express()
 
 
@@ -17,25 +18,42 @@ app.get("/quiz/questions",(req,res)=>{
     res.send(quiz[randIdx]);
 })
 
-app.get("/scores/leaderboeard",(req,res)=>{
-    res.send(score);
+app.get("/scores/easy", (req, res) => {
+    const easyScores = scores.filter(score => score.difficulty === "easy").sort((a, b) => b.score - a.score); 
+    res.status(200).send(easyScores);
 })
 
-app.post("/score/update", (req,res)=>{
-// <<<<<<< score
-//     const request = req.body[0];
-//     if (request.student && isNaN(request.student)){
-//         //check if the student is already present in the score.json file
-//         //if yes, add request.score to their score
-//         //else update score.jason with req.body
-//     }
-//     else {
-//         res.status(406).json({message: "enter a valid name please"})
-//     }
-// =======
-//     const student = req.body;
+app.get("/scores/medium", (req, res) => {
+    const easyScores = scores.filter(score => score.difficulty === "medium").sort((a, b) => b.score - a.score); 
+    res.status(200).send(easyScores);
+})
 
-// >>>>>>> main
+app.get("/scores/hard", (req, res) => {
+    const easyScores = scores.filter(score => score.difficulty === "hard").sort((a, b) => b.score - a.score); 
+    res.status(200).send(easyScores);
+})
+
+app.get("/scores/user/:username", (req, res) => {
+    const username = req.params.username; 
+    const userScore = scores.filter(score => score.username === username); 
+    if (userScore) {
+        res.status(200).send(userScore); 
+    } else {
+        res.status(404).send({error: `Score of ${username} not found`})
+    }
+})
+
+
+app.post("/score", (req, res) => {
+    const newScore = req.body; 
+    if (newScore.username && newScore.score && newScore.difficulty) {
+        res.status(201).send(newScore);
+    } else {
+        res.status(422).send({error:"Some or all of the post info are missing."})
+        
+    }
+    
+
 })
 
 module.exports = app;
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const port = 8080;
 
 app.listen(port,()=>{
-    console.log(`app is listenig on port ${port}`);
+    console.log(`app is listening on port ${port}`);
 })
