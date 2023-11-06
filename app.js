0586afb2a0bbb484a4696846fa4baecb3bc8ac1a
Esham <Esham156@gmail.com>
2023-11-06 12:02:22 +0000

    add endpoint to random question & answer from country.jason

--- app.js
+++ app.js
@@ -3,4 +3,19 @@ const cors = require("cors")
 const quiz = require('./quiz.json')
 const app = express()
 
+app.get("/quiz/questions",(req,res)=>{
+    const randIdx = Math.floor(Math.random()*(quiz.length));
+    res.send(quiz[randIdx]);
+})
+
+app.get("/scores/leaderboeard",(req,res)=>{
+    res.send(score);
+})
+
+app.post("/score/update", (req,res)=>{
+    const student = req.body;
+    if ()
+
+})
+
 module.exports = app;
